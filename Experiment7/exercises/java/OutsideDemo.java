d76087f56d34eb8d0ddc9c0d52faef1fdcae1504
Bushra Fatima <bushrafatima9810@gmail.com>
2025-11-18 21:02:02 +0530

    Add files via upload

---
+++ Experiment7/exercises/java/OutsideDemo.java
@@ -0,0 +1,8 @@
+import exercises.java.PackageDemo;   
+public class OutsideDemo {
+    public static void main(String[] args) {
+        PackageDemo demo = new PackageDemo();
+        demo.display();
+        System.out.println("Bushra 24csu265");
+    }
+}
